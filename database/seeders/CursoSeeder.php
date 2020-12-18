4bfc41ee743c1ed5e12df7dfe12a08f17fe2790f
mimismosv <oicguevara@gmail.com>
2020-12-17 21:42:07 -0600

    DataBase Seed

--- database/seeders/CursoSeeder.php
+++ database/seeders/CursoSeeder.php
@@ -3,18 +3,17 @@
 namespace Database\Seeders;
 use App\Models\Curso;
 use Illuminate\Database\Seeder;
-use App\Models\User;
 
-class DatabaseSeeder extends Seeder
+class CursoSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        User::factory(10)->create();
+        //
         Curso::factory(50)->create();
 
     }
